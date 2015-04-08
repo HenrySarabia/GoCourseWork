7a500c7bc1e404de49d5ce4a1110eb0c1d0ee4f4
HenrySarabia <HenrySarabia1993@gmail.com>
2015-04-08 00:50:49 -0700

    Finished Key and Query sections

--- springBreakSite/springBreakSite.go
+++ springBreakSite/springBreakSite.go
@@ -22,7 +22,7 @@ var homeTemplate = template.Must(template.ParseFiles("home.html"))
 
 //Creating a function that, when called, will return a new key with a consistent parent
 func defaultKey(c appengine.Context) *datastore.Key {
-	return datastore.NewKey(c, "userInput", "default", 0, nil)
+	return datastore.NewKey(c, "userInput", "defaultParent", 0, nil)
 }
 
 //The initialization function that will call handlers based on the url
@@ -33,6 +33,7 @@ func init() {
 
 //This is the main handler that will execute the home page HTML template
 func homeHandler(w http.ResponseWriter, r *http.Request){
+	//Declaring context and user variables
 	c := appengine.NewContext(r)
 	u := user.Current(c)
 
@@ -50,7 +51,16 @@ func homeHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Location 2", "Sample")
 	w.WriteHeader(http.StatusFound)
 
-	err := homeTemplate.Execute(w, u)
+	//Query
+	q := datastore.NewQuery("userInput").Ancestor(defaultKey(c)).Order("-Date").Limit(15)
+	inputList := make([]userInput, 0, 10)
+	if _, err := q.GetAll(c, &inputList); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//Execute home.html template
+	err := homeTemplate.Execute(w, inputList) //Pass in inputList and rewrite HTML
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -76,8 +86,16 @@ func messageHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusFound)
 
 	input := userInput{
-		Content: r.FormValue("")
+		Author: u.String(),
+		Content: r.FormValue("messageString"),
+		Date: time.Now(),
 	}
-}
 
-//Still need to add two more handlers and the queries. About 50% done.
+	key := datastore.NewIncompleteKey(c, "userInput", defaultKey(c))
+	_, err := datastore.Put(c, key, &input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
+}
